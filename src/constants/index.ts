02d050becc08cd1440c27b67e9be50c1ded8c9c5
saranatour1 <saranatourcontact@gmail.com>
2024-03-09 02:21:51 +0200

    chore: my constants.

---
+++ src/constants/index.ts
@@ -0,0 +1,46 @@
+/**
+ * @description The purpose of this file is to make
+ */
+
+const mainHeading = `The un-professional version of My Portfolio`;
+
+/**
+ * @description the unprofessional version of the about me;
+ */
+const whatIDo = `I am Sara (S-A-R-A), A Full-stack developer with a background in electrical engineering. I career shifted last year (2023 to be exact),
+  I have a few reasons that made me decide to do this career shift, one of which is me being petty that in my graduation project I wasn't allowed to experiment 
+  with creating my own analytics website, for my data, and it was a thing that interested me for a couple of years to look into.
+  its an interesting new path, with a lot of new challenges, no path has a no obstacles, and what interests me about this one is that you always have something to learn,
+  and you'll always find a community to answer your questions if you have any!`;
+
+/**
+ * @description TL;DR
+ */
+const TLDR = `Sara, 24, Full-stack developer, For more information check my Github account.`;
+
+/**
+ * @description my full-stack projects
+ */
+const fs:object[] = [];
+
+/**
+ * @description my frontend mentor projects
+ */
+const fe:object[] =[];
+
+/**
+ * @description my other interesting projects
+ */
+const op:object[] = [];
+
+/**
+ * @description my other interests
+ */
+const myInterests:object[] = [];
+
+/**
+ * @description my socials, 
+ */
+const mySocials:object[] = [];
+
+export { mainHeading, whatIDo, TLDR, fs, fe, op, myInterests, mySocials};
